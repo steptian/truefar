3e9c510d579b476dd2f743912793e7cd441db859
Steptian <alter.it@gmail.com>
2016-09-08 17:18:07 +0800

    Create s.js

---
+++ s.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a0c0746f4180d93940414b6c6e732199";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
